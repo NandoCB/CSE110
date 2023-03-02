eddcd3e0e1fdcfa483c0da9f6367e3b0a06712fb
FernandoDCardozoBraga <fer.car.br@gmail.com>
2023-03-02 15:08:36 -0300

    w9_on work

--- w9/w9_assignment_milestone.py
+++ w9/w9_assignment_milestone.py
@@ -11,6 +11,7 @@ position = 0
 print('Welcome to the Shopping Cart Program!')
 print('')
 while manage_cart == True:
+
     print("""Please select one of the following:
     1. Add item
     2. View cart
@@ -22,7 +23,6 @@ while manage_cart == True:
     if enter_action == 1:
         while addin_item_dashboard == True:
              
-          
             item = input('Enter item to add: ')
             item_list.append(item.capitalize())
             price = float(input('Enter item price: '))
@@ -43,40 +43,44 @@ while manage_cart == True:
         
     elif enter_action == 2:
         print('The contents of the shopping cart are:')
-        print("+--------------------+------------+-------------+-----------+")
-        print("|ITEM                |QTY.        |PRICE        |AMOUNT     |")
-        print("+--------------------+------------+-------------+-----------+")
-             
-        for a, b, c, d in zip(item_list, item_price, item_quantity, amount_list):
+        print("+------------+--------------------+------------+-------------+-----------+")
+        print("|ITEM NUMBER |ITEM                |QTY.        |PRICE        |AMOUNT     |")
+        print("+------------+--------------------+------------+-------------------------+")
+        counter = 1     
+        for a, b, c, d in zip (item_list, item_quantity, item_price, amount_list):
             
-            print("|{:<20}|{:>12.2f}|${:>12.2f}|${:>10.2f}|".format(a, b, c, d))
+            print("|  {:10}|{:<20}|{:>12.2f}|${:>12.2f}|${:>10.2f}|".format(counter, a, b, c, d))
+            counter += 1
  
-        print("+--------------------+------------+-------------+-----------+")
+        print("+------------+--------------------+------------+-------------------------+")
         for amount in amount_list:
-             total += amount
+            total += amount
+        
     
-        print("|--------------------|------------|TOTAL:       |${:>10.2f}|".format(total))
-        print("+--------------------+------------+-------------+-----------+")
+        print("|------------|--------------------|------------|TOTAL:       |${:>10.2f}|".format(total))
+        print("+------------+--------------------+------------+-------------------------+")
     
     elif enter_action == 3:
-        delete_item = input('Enter the name of the item to delete: ')
-        delete_item = delete_item.capitalize()
-        if delete_item in item_list:
-            position = item_list.index(delete_item)
+        delete_item = input('Enter the number of the item to delete: ')
+        print()
+        if delete_item.isdigit() and int(delete_item) <= counter:
+            position = int(delete_item) - 1
             del item_list[position]
             del item_price[position]
             del item_quantity[position]
             del amount_list[position]
         
-            print(f'{delete_item} deleted successfully')
+            print(f'* {delete_item} * deleted successfully')
         else:
              print('The article does not exist')
+             print()
     
     elif enter_action == 4:
         for amount in amount_list:
             total += amount
-            print(f'The total price of the items in the shopping cart is ${total:.2f}')
-    
+        print(f'The total price of the items in the shopping cart is ${total:.2f}')
+        print()
+            
     elif enter_action == 5:
         user_exit = input('Are you sure you want to exit the cart? [YES or NO]: ')
         user_exit = user_exit.lower()
@@ -84,3 +88,7 @@ while manage_cart == True:
              sys.exit('Thank you. Goodbye.')
         else:
              manage_cart = True
+    
+    else:
+        print('Invalid input error, please enter a number between 1 and 5')
+        manage_cart == True
